903e5cd7c1352df8fa98c6e39ea45e719b685971
analuciagalindo1719 <analuciagalindo625@gmail.com>
2026-05-26 16:41:21 -0500

    Agrega funcion catalogo

--- main.py/main.py
+++ main.py/main.py
@@ -3,3 +3,7 @@ print("1. Ver catálogo de productos")
 print("2. Agregar al carrito")
 print("3. Ver carrito de compras")
 print("4. Salir")
+
+def mostrar_catalogo():
+    print("1. Computadora - $800")
+    print("2. Celular - $400")
